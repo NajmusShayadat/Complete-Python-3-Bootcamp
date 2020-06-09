9541e0449033340aa4243a0c971438bb627544a4
NajmusShayadat <najmus.bappy@gmail.com>
2020-06-09 13:53:38 +0600

    UDemy Chapter 3: Udemy Python 3 Bootcamp Milestone Project 1.
    Added docstring for each function and useful comments

--- 04-Milestone Project - 1/04Project_01_Tic_Tac_Toe.py
+++ 04-Milestone Project - 1/04Project_01_Tic_Tac_Toe.py
@@ -20,9 +20,15 @@
 
 
 def show_board(v_p, v_g):
+    """
+    DOCSTRING: Print two boards, One showing available positions, another one showing the game progress!
+    :param v_p: board one, position list
+    :param v_g: board two, game display list
+    """
     # clear_output()    # to run in jupyter notebook
-    print('\n' * 100)   # to run in any IDE other than Jupyter notebook
+    print('\n' * 100)  # to run in any IDE other than Jupyter notebook
 
+    # print the board
     print(" Available\t Game board\n positions\t  display!\n")
     print(f" {v_p['p7']} | {v_p['p8']} | {v_p['p9']} \t  {v_g['g7']} | {v_g['g8']} | {v_g['g9']}")
     print("-----------\t -----------")
@@ -32,6 +38,11 @@ def show_board(v_p, v_g):
 
 
 def initiate_game(preset):
+    """
+    DOCSTRING:  a list of 10 elements becomes a list of 9 elements (X and O in sequence according to player 1's choice)
+    :param preset: a list, containing 10 moves ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O']
+    :return: a lis of 9 moves in sequence
+    """
     p1symbol = input("\nPlayer 1, Pick X or O and press enter: ")
     if 'X' in p1symbol.upper():
         preset.pop(-1)
@@ -46,6 +57,14 @@ def initiate_game(preset):
 
 
 def use_input(loc, symbols, v_p, v_g):
+    """
+    DOCSTRING: asks for input position checks the input validity
+    and shows the board after modifying the position list and game display list
+    :param loc: an integer representing the turn number.
+    :param symbols: a list containing 9 elements of X and O in sequence
+    :param v_p: board one, position list
+    :param v_g: board two, game display list
+    """
     inp = int(input(f"Player {(loc % 2 != 0) + 1} ({symbols[loc]}), please enter your next position: "))
     if inp in v_p.values():
         pKey = 'p' + str(inp)
@@ -59,6 +78,11 @@ def use_input(loc, symbols, v_p, v_g):
 
 
 def check_win(vg):
+    """
+    DOCSTRING: checks all the winning conditions from game board display list
+    :param vg: game board display list
+    :return: Boolean
+    """
     return (vg['g1'] == vg['g2'] == vg['g3'] != ' ') or (vg['g4'] == vg['g5'] == vg['g6'] != ' ') or (
             vg['g7'] == vg['g8'] == vg['g9'] != ' ') or (vg['g1'] == vg['g4'] == vg['g7'] != ' ') or (
                    vg['g2'] == vg['g5'] == vg['g8'] != ' ') or (vg['g3'] == vg['g6'] == vg['g9'] != ' ') or (
@@ -66,41 +90,51 @@ def check_win(vg):
 
 
 def check_move(vg):
+    """
+    DOCSTRING: Checks for blank space in game board display list.
+    :param vg: game board display list.
+    :return: Boolean
+    """
     return ' ' in vg.values()
 
 
 def replay():
+    """
+    DOCSTRING: takes an input for yes or no and checks only the first letter is lower case y.
+    :return: Boolean
+    """
     return input('Play again? Enter Yes or No: ').lower().startswith('y')
 
 
 while True:
     # clear_output()    # to run in jupyter notebook
-    print('\n' * 100)  # to run in any IDE other than Jupyter notebook
+    print('\n' * 100)   # to run in any IDE other than Jupyter notebook
     print("Welcome to Tic Tac Toe!")
 
-    # Declare variables
+    # Declare variables dictionaries representing two display board
     positionboard = {'p1': 1, 'p2': 2, 'p3': 3, 'p4': 4, 'p5': 5, 'p6': 6, 'p7': 7, 'p8': 8, 'p9': 9}
     gameboard = {'g1': ' ', 'g2': ' ', 'g3': ' ', 'g4': ' ', 'g5': ' ', 'g6': ' ', 'g7': ' ', 'g8': ' ', 'g9': ' '}
+    # a set of 10 moves
     moveset = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X', 'O']
 
-    turns = initiate_game(moveset)  # all 9 moves in a sequence depending on the initial pick 'X' or 'O'
-    show_board(positionboard, gameboard)
+    turns = initiate_game(moveset)          # all 9 moves in a sequence depending on the initial pick 'X' or 'O'
+    show_board(positionboard, gameboard)    # Displays 2 boards
 
     game_on = True
     while game_on:
-        for i in range(9):
-            wincheck = check_win(gameboard)
-            if wincheck:  # checking if winning condition is met
+        for i in range(9):                          # max 9 moves
+            wincheck = check_win(gameboard)         # checking if winning condition is met
+            if wincheck:
                 print(f"\n\n\tCongratulations!\n\tPlayer {1 + int((i - 1) % 2 != 0)} wins!")
                 game_on = False
                 break
             else:
-                movecheck = check_move(gameboard)
+                movecheck = check_move(gameboard)   # checking if moves available
                 if movecheck:
-                    use_input(i, turns, positionboard, gameboard)
+                    use_input(i, turns, positionboard, gameboard)   # asking for input again
                 else:
                     print('\n\n Match draw')
                     game_on = False
                     break
-    if not replay():
+    if not replay():    # ask for replay the game
         break
